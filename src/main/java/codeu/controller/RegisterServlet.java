df9fd0f9f215089f78e99d3fb3e6c8b04a9f3c3b
Ken Wang <fengfeng8867dd@hotmail.com>
2018-03-22 16:55:31 -0600

    Deleted another extra new line

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -3,12 +3,10 @@ package codeu.controller;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.UUID;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import codeu.model.data.User;
 import codeu.model.store.basic.UserStore;
 
