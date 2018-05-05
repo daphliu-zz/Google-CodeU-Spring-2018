b14bebd3000211d107478a668bbc199f9f9b1acb
Daphne Liu <daphnel15113455@gmail.com>
2018-05-04 17:12:08 -0700

    Added chat to offline page

--- src/main/webapp/sw.js
+++ src/main/webapp/sw.js
@@ -1,10 +1,12 @@
 // Service Worker
 
+const CACHE_NAME = "codeu-chat"
+
 /**
  * Caches resources that don't change often (ex. CSS, images)
  */
 async function installDeps() {
-    const cache = await caches.open('codeu-chat');  // codeu-chat is arbitrary name of chat 
+    const cache = await caches.open(CACHE_NAME);  // codeu-chat is arbitrary name of chat 
     return cache.addAll([
         "/css/main.css",
         "/offline.html", // General error page
@@ -20,7 +22,9 @@ async function installDeps() {
 async function load(request) {
     try {
         // Attempt to load from internet
-        return await fetch(request);
+        const response = await fetch(request);
+        await storeConversation(request, response);  // Conversation is put in try block b/c it updates constantly
+        return response;
     } catch (err) {
         // If no internet, return from cache instead
         const response = await caches.match(request); 
@@ -42,6 +46,21 @@ async function findOfflinePage(request) {
             return caches.match(url.pathname + "?offline");
         default:
             return caches.match("/offline.html");
+    }
+}
+
+/**
+ * If the request was for a conversation page, clone the response and store
+ * it in the cache.
+ * @param {Request} request from user
+ * @param {Response} response from server
+ */
+async function storeConversation(request, response) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/conversations" || url.pathname.startsWith("/chat")) {
+        const cache = await caches.open(CACHE_NAME)
+        cache.put(request, response.clone());
     }
 }
 
