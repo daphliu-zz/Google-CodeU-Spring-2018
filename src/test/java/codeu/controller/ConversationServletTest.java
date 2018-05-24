fa0554bf34bbeab9705aaea9444b4de2a3c1058b
svelasco7 <velasco1315@gmail.com>
2018-05-24 11:22:40 -0700

    currently commented out test for what a logged in user views on conversations page, still need to add tests

--- src/test/java/codeu/controller/ConversationServletTest.java
+++ src/test/java/codeu/controller/ConversationServletTest.java
@@ -71,7 +71,6 @@ public class ConversationServletTest {
         new Conversation(UUID.randomUUID(), UUID.randomUUID(), "test_conversation", Instant.now()));
     Mockito.when(mockConversationStore.getAllConversations()).thenReturn(fakeConversationList);
 //    conversationServlet.doGet(mockRequest, mockResponse);
-
   //  Mockito.verify(mockRequest).setAttribute("conversations", fakeConversationList);
     //Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
   }
