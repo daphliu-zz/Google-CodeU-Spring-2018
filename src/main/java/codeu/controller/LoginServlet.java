23cdf724aaaabb0607e003565d949870a053b083
Daphne Liu <daphnel15113455@gmail.com>
2018-05-23 23:14:10 -0400

    Fixed casting Text to String problem

--- src/main/java/codeu/controller/LoginServlet.java
+++ src/main/java/codeu/controller/LoginServlet.java
@@ -30,8 +30,8 @@ public class LoginServlet extends HttpServlet {
   private UserStore userStore;
 
   /**
-   * Set up state for handling login-related requests. This method is only called when running in a
-   * server, not when running in a test.
+   * Set up state for handling login-related requests. This method is only called
+   * when running in a server, not when running in a test.
    */
   @Override
   public void init() throws ServletException {
@@ -40,31 +40,30 @@ public class LoginServlet extends HttpServlet {
   }
 
   /**
-   * Sets the UserStore used by this servlet. This function provides a common setup method for use
-   * by the test framework or the servlet's init() function.
+   * Sets the UserStore used by this servlet. This function provides a common
+   * setup method for use by the test framework or the servlet's init() function.
    */
   void setUserStore(UserStore userStore) {
     this.userStore = userStore;
   }
 
   /**
-   * This function fires when a user requests the /login URL. It simply forwards the request to
-   * login.jsp.
+   * This function fires when a user requests the /login URL. It simply forwards
+   * the request to login.jsp.
    */
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException, ServletException {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
     request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
   }
 
   /**
-   * This function fires when a user submits the login form. It gets the username and password from
-   * the submitted form data, checks that they're valid, and either adds the user to the session so
-   * we know the user is logged in or shows an error to the user.
+   * This function fires when a user submits the login form. It gets the username
+   * and password from the submitted form data, checks that they're valid, and
+   * either adds the user to the session so we know the user is logged in or shows
+   * an error to the user.
    */
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws IOException, ServletException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
     String username = request.getParameter("username");
     String password = request.getParameter("password");
 
@@ -72,7 +71,7 @@ public class LoginServlet extends HttpServlet {
       User user = userStore.getUser(username);
       if (BCrypt.checkpw(password, user.getHashedPassword())) {
         request.getSession().setAttribute("user", username);
-        if (user.getAdminStatus() && user.getAdminStatus() != null) {
+        if (user.getAdminStatus()) {
           request.getSession().setAttribute("is_admin", true);
           response.sendRedirect("/adminStats");
           return;
--- src/main/java/codeu/model/store/persistence/PersistentDataStore.java
+++ src/main/java/codeu/model/store/persistence/PersistentDataStore.java
@@ -164,7 +164,7 @@ public class PersistentDataStore {
         UUID conversationUuid = UUID.fromString((String) entity.getProperty("conv_uuid"));
         UUID authorUuid = UUID.fromString((String) entity.getProperty("author_uuid"));
         Instant creationTime = Instant.parse((String) entity.getProperty("creation_time"));
-        String content = ((Text) entity.getProperty("content")).getValue();
+        String content = (String) entity.getProperty("content"); 
         Message message = new Message(uuid, conversationUuid, authorUuid, content, creationTime);
         messages.add(message);
       } catch (Exception e) {
@@ -208,7 +208,7 @@ public class PersistentDataStore {
     messageEntity.setProperty("uuid", message.getId().toString());
     messageEntity.setProperty("conv_uuid", message.getConversationId().toString());
     messageEntity.setProperty("author_uuid", message.getAuthorId().toString());
-    messageEntity.setProperty("content", new Text(message.getContent()));
+    messageEntity.setProperty("content", message.getContent()); 
     messageEntity.setProperty("creation_time", message.getCreationTime().toString());
     datastore.put(messageEntity);
   }
